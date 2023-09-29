e24b433bb22a7ca967629557ad3cce2551e37d30
Shubham Desale <shubhamhanuantpatil@gmail.com>
2023-09-29 18:02:46 +0530

    added the files

---
+++ script.js
@@ -0,0 +1,63 @@
+
+let crsr = document.querySelector('.crsr')
+let main = document.querySelector('main')
+
+ 
+main.addEventListener("mousemove",(e)=>{
+    gsap.to(crsr,{
+     left:e.x,
+        top:e.y,
+        opacity:1,
+        scale:1
+        
+    })
+    
+
+ })
+
+
+var tl = gsap.timeline();
+
+tl.from("nav",{
+    y:100,
+    opacitya:0,
+    scale:0,
+    duration:.5,
+    delay:1
+})
+tl.from(".logo h1, .menu",{
+
+    y:-100,
+    duration:.5,
+    delay:0.4,
+    stagger:0.3,
+    opacity:0
+
+})
+
+tl.from(".left p,.left h1",{
+    x:-90,
+    opacity:0,
+    stagger:0.3
+})
+tl.from(".right img",{
+    scale:0.7,
+    opacity:0,
+    
+})
+
+gsap.from(".page2 .info h1,.info p",{
+
+    x:-100,
+    opacity:0,
+    delay:3,
+    duration:2,
+    stagger:0.4,
+   srcollTrigger:{
+    trigger:".page2 .info h1",
+    srcoller:"body"
+   }
+})
+
+
+
